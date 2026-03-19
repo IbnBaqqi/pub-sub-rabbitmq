5246a3faa45dbd36723621dc86962012bd5e1073
Abdulbaki <Salausmart132@gmail.com>
2026-03-19 03:21:22 +0200

    refactor main to handle rabbitmq conn handling & graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+	
 	fmt.Println("Starting Peril server...")
+
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("unable to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+	fmt.Println("RabbitMQ connection closed.")
 }
