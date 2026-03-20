71e742f140f44f9afa492c35f582e9d69baf8357
Abdulbaki <Salausmart132@gmail.com>
2026-03-20 16:03:51 +0200

    Implement message publishing to RabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,8 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/ibnbaqqi/pub-sub-rabbitmq/internal/pubsub"
+	"github.com/ibnbaqqi/pub-sub-rabbitmq/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -21,6 +23,22 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	amqpCh, err := conn.Channel()
+	if err != nil {
+		fmt.Printf("unable to create channel: %v", err)
+	}
+
+	err = pubsub.PublishJSON(
+		amqpCh,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{IsPaused: true},
+	)
+	if err != nil {
+		log.Printf("could not publish time: %v", err)
+	}
+	fmt.Println("Pause message sent!")
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
