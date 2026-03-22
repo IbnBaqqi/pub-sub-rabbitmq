96a2ddd0b6c5986e0d1f3d067ba7123686b0d70c
Abdulbaki <Salausmart132@gmail.com>
2026-03-22 20:02:54 +0200

    client: connect to RabbitMQ and bind pause queue on startup
    Dials RabbitMQ, prompts for a username via ClientWelcome, then declares and
    binds a transient queue (<username>.pause) to the peril-direct exchange.
    Blocks until SIGINT/SIGTERM and logs a clean shutdown message.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ibnbaqqi/pub-sub-rabbitmq/internal/gamelogic"
+	"github.com/ibnbaqqi/pub-sub-rabbitmq/internal/pubsub"
+	"github.com/ibnbaqqi/pub-sub-rabbitmq/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+
 	fmt.Println("Starting Peril client...")
+	
+	// client connection
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("unable to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	username, _ := gamelogic.ClientWelcome()
+
+	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
+
+	// declare and bind the connection through exhange to the queue
+	pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		queueName,
+		routing.PauseKey,
+		pubsub.TransientQueue,
+	)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+	fmt.Println("RabbitMQ client connection closed.")
 }
