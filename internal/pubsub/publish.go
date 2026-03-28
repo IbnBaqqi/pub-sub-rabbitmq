42ba0cba75abe426c01a649c59535217ec00d334
Abdulbaki <Salausmart132@gmail.com>
2026-03-28 23:57:28 +0200

    docs(pubsub): add missing doc comment to PublishJSON

--- internal/pubsub/publish.go
+++ internal/pubsub/publish.go
@@ -7,6 +7,7 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+// PublishJSON handles publishing message to exchange
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 
 	data, _ := json.Marshal(val)
