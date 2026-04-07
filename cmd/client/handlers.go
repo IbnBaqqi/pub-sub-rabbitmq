b18a01382373f028f02959d189193cfc4fd7d1e7
Abdulbaki <Salausmart132@gmail.com>
2026-04-07 23:21:37 +0300

    Update handlers to return Acktype and handle move outcomes with proper ack/nack

--- cmd/client/handlers.go
+++ cmd/client/handlers.go
@@ -4,25 +4,37 @@ import (
 	"fmt"
 
 	"github.com/ibnbaqqi/pub-sub-rabbitmq/internal/gamelogic"
+	"github.com/ibnbaqqi/pub-sub-rabbitmq/internal/pubsub"
 	"github.com/ibnbaqqi/pub-sub-rabbitmq/internal/routing"
 )
 
 // handlerPause returns a handler that updates the game state when a pause/resume
 // message is received
-func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
+func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.Acktype {
 
-	return func(ps routing.PlayingState) {
+	return func(ps routing.PlayingState) pubsub.Acktype {
 		defer fmt.Print("> ")
 		gs.HandlePause(ps)
+		return pubsub.Ack
 	}
 }
 
 // handlerMove returns a handler that processes incoming army move messages
 // from other players and updates the local game state accordingly.
-func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.Acktype {
 
-	return func(armyMove gamelogic.ArmyMove) {
+	return func(move gamelogic.ArmyMove) pubsub.Acktype {
 		defer fmt.Print("> ")
-		gs.HandleMove(armyMove)
+		moveOutcome := gs.HandleMove(move)
+		switch moveOutcome {
+		case gamelogic.MoveOutcomeSamePlayer:
+			return pubsub.NackDiscard
+		case gamelogic.MoveOutComeSafe:
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeMakeWar:
+			return pubsub.Ack
+		}
+		fmt.Println("error: unknown move outcome")
+		return pubsub.NackDiscard
 	}
 }
